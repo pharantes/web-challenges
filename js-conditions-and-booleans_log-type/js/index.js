504497cedd79f7c3d2ff4ec540be0bbfc0333f73
pharantes <p.arantes@protonmail.com>
2024-12-17 23:48:01 +0100

    challenge logtype done

---
+++ js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,38 @@
+let data;
+data = "null"
+function checkTypeofData() {
+    if (data === null) {
+        return console.log(null)
+    }
+    switch (typeof data) {
+        case "string":
+            console.log("string")
+            break;
+        case "number":
+            console.log("number")
+            break;
+        case "object":
+            console.log("object")
+            break;
+        case "array":
+            console.log("object")
+            break;
+        case "null":
+            console.log("null")
+            break;
+        case "undefined":
+            console.log("undefined")
+            break;
+        case "boolean":
+            console.log("boolean")
+            break;
+        case "bigint":
+            console.log("bigint")
+            break;
+        case "symbol":
+            console.log("symbol")
+            break;
+    }
+}
+checkTypeofData(data)
+
