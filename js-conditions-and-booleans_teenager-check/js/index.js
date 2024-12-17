1c01bdda0a535a5406358c639859fc709755f98a
pharantes <p.arantes@protonmail.com>
2024-12-17 23:19:06 +0100

    challenge-teenager-check done

--- js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -10,4 +10,10 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+  if (age > 12 && age < 18) {
+    output.textContent = "You are a teen."
+  }
+  else {
+    output.textContent = "You are not a teen."
+  }
 });
