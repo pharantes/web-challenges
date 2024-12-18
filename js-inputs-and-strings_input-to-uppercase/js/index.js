52899bc1a998295fc37c5229b85831777d58228b
Pedro <p.arantes@protonmail.com>
2024-12-18 14:49:55 +0100

    challenge js uppercase done

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,6 @@
 console.clear();
+const input = document.querySelector("[data-js=first-input]");
+const button = document.querySelector("[data-js=button-uppercase]");
+button.addEventListener("click", () => {
+  input.value = input.value.toUpperCase();
+});
