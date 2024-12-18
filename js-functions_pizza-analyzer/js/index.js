f729e7c3cb5d8f7092d4ac0b1e32259390aa35db
Pedro <p.arantes@protonmail.com>
2024-12-18 12:12:18 +0100

    trying to fix this but its impossible

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -7,25 +7,27 @@ const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
+let pizzaSize1;
+let pizzaSize2;
 
 pizzaInput1.addEventListener("input", () => {
   // write your code here
-  let pizzaSize1;
-  let pizzaSize2;
+
   pizzaSize1 = pizzaInput1.value;
   pizzaSize2 = pizzaInput2.value;
 
-  updatePizzaDisplay(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
   // write your code here
-  let pizzaSize1;
-  let pizzaSize2;
+
   pizzaSize1 = pizzaInput1.value;
   pizzaSize2 = pizzaInput2.value;
 
-  updatePizzaDisplay(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
@@ -43,14 +45,15 @@ calculatePizzaGain(pizzaSize1, pizzaSize2);
 // Task 2
 // define the function updatePizzaDisplay here
 const updatePizzaDisplay = (pizzaElement, newSize) => {
-  pizzaElement.style.width = newSize + "px";
+  pizzaElement.style.width = (newSize / 24) * 100 + "px";
 };
 // Task 3
 // define the function updateOutputColor here
 const updateOutputColor = (size1, size2) => {
   if (size1 > size2) {
-    outputSection.style.backgroundColor = `var(--red)`;
-  } else if (size2 > size1) {
-    outputSection.style.backgroundColor = `var(--green)`;
+    outputSection.style.backgroundColor = "var(--red)";
+  }
+  if (size2 > size1) {
+    outputSection.style.backgroundColor = "var(--green)";
   }
 };
