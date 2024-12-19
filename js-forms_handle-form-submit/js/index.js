f9cf9304a601800825bee5a030361f617590348f
pharantes <p.arantes@protonmail.com>
2024-12-19 11:07:07 +0100

    challenge-js-forms-handle-form-submit done

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,25 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // [...] handle form data
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    console.log("The age-badness-sum of", data.firstName, "is", data.age + data.badness);
+    event.target.reset();
+});
+
+// OUTPUT
+// {
+//  age: "18"
+//  badness: "32"
+//  complaint: "nothing"
+//  details: ""
+//  email: ""
+//  firstName: "aaaaaaa"
+//  lastName: ""orderDate: ""
+//  [[Prototype]]: Object
+// }
+// The age-badness-sum of aaaaaaa is -14
