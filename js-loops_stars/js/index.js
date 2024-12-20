3894d8ac09a496909633702c5c6b75693c970643
Pedro <p.arantes@protonmail.com>
2024-12-20 15:39:16 +0100

    challenge for loop stars done

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,13 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
+  for (let i = 1; i <= filledStars; i++) {
+    let star = document.createElement("img");
+    star.setAttribute("src", "./assets/star-filled.svg");
+    starContainer.append(star);
+  }
+  for (let index = 1; index <= 5 - filledStars; index++) {
+    let star = document.createElement("img");
+    star.setAttribute("src", "./assets/star-empty.svg");
+    starContainer.append(star);
+  }
 
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(3);
