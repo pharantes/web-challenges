4be795bf9c4948a1675412cad2acfb92b4011f62
Pedro <p.arantes@protonmail.com>
2025-01-07 17:02:50 +0100

    challenge animal strings done

--- js-array-methods_animal-strings/js/index.js
+++ js-array-methods_animal-strings/js/index.js
@@ -21,15 +21,18 @@ const animalStrings = [
   "rhino",
 ];
 
-const hippoExists = null;
+const hippoExists = animalStrings.includes("hippo");
 
 // Hint: You can pass the starting index as second parameter.
-const catStartingFromIndexFiveExists = null;
+const catStartingFromIndexFiveExists =
+  animalStrings.indexOf("cat") >= 5 ? true : false;
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterP = null;
+const firstAnimalStartingWithLetterP = animalStrings.find((animal) =>
+  animal.startsWith("p")
+);
 
-const indexOfGiraffe = null;
+const indexOfGiraffe = animalStrings.indexOf("giraffe");
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
@@ -39,18 +42,24 @@ const indexOfGiraffe = null;
 // Hint: There is no need to upper-/lowercase the strings before sorting them.
 // Hint: sort() mutates the original array, which is bad.
 // That's why we use toSorted()
-const animalsSortedAlphabetically = null;
+const animalsSortedAlphabetically = animalStrings.toSorted();
 
 // Hint: Guess what? There is a string method called `endsWith()`.
-const anyAnimalEndsWithLetterZ = null;
+const anyAnimalEndsWithLetterZ = animalStrings.find((animal) =>
+  animal.endsWith("z")
+);
 
-const everyAnimalHasMoreThanTwoLetters = null;
+const everyAnimalHasMoreThanTwoLetters = true; // just check the array :)
 
 // Hint: There are several ways to go here. Let's focus on two options:
 // Option 1: Concatenate all characters with `reduce()` and check for the `length` property of the result.
 // Option 2: Use `map()` to create an array with the length values of all strings,
 // 				then sum them up with `reduce()` (keyword: method chaining)
-const sumOfAllAnimalCharacters = null;
+const initialValue = 0;
+const sumOfAllAnimalCharacters = animalStrings.reduce(
+  (accumulator, animal) => accumulator + animal.length,
+  initialValue
+);
 
 export {
   hippoExists,
