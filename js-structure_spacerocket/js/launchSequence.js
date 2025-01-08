885b94beb69f8c2190916c31ea52c2ac12e453ef
Pedro <p.arantes@protonmail.com>
2025-01-08 15:53:44 +0100

    added fetch challenges

--- js-structure_spacerocket/js/launchSequence.js
+++ js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,14 @@
 // Implement the launch sequence function here and export it as the default export.
+import { loadPayload } from "./core/load.js";
+import { fuel } from "./core/fuel.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+
+loadPayload(NFSAT);
+loadPayload(FISHSAT);
+fuel();
+countdown();
+liftoff();
+deployPayload();
