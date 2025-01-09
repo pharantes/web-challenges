3eeabe74924afae0b291ef6b3122724aff556524
Pedro <p.arantes@protonmail.com>
2025-01-09 10:46:46 +0100

    challenge error calc done

--- js-error-handling_calculator/js/index.js
+++ js-error-handling_calculator/js/index.js
@@ -2,19 +2,22 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
-
+const errorParagraph = document.querySelector(".error");
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        return error;
+      } else return a / b;
+    } catch (error) {
+      errorParagraph.innerHTML = "Can't divide by 0";
+      return a / b;
     }
-    return a / b;
   },
 };
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const firstNumber = Number(event.target.firstNumber.value);
