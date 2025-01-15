fbc8fb5c7abf21145441c014738cec6d15816edb
Pedro <p.arantes@protonmail.com>
2025-01-15 14:58:46 +0100

    challenge nesting fragments done

--- react-nesting_fragments/src/App.jsx
+++ react-nesting_fragments/src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./styles.css";
 import Box from "./components/Box";
+import { Fragment } from "react";
 
 export default function App() {
   return (
     <main className="flex-container">
+      <Boxes />
+    </main>
+  );
+}
+import React from 'react'
+
+export default function Boxes() {
+  return (
+    <Fragment>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
-  );
+    </Fragment>
+  )
 }
