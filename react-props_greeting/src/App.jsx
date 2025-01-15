aa6aa10a245d8b4b050219c61877bcaa51c533f0
Pedro <p.arantes@protonmail.com>
2025-01-15 11:24:07 +0100

    challenge react props greeting done

--- react-props_greeting/src/App.jsx
+++ react-props_greeting/src/App.jsx
@@ -1,5 +1,5 @@
 import { Greeting } from "./components/Greeting";
-
+// Alignment
 export default function App() {
   return <Greeting name="Bla bla" />;
 }
--- react-props_greeting/src/components/Greeting.jsx
+++ react-props_greeting/src/components/Greeting.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// aligment
 export function Greeting({ name }) {
   return <div>Hello, {name == "Coach" ? "Coach" : name}</div>;
 }
