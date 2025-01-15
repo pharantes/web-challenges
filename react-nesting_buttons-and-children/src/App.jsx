4dbcde1dd4436c5e2babc47cad90f18f706458a6
Pedro <p.arantes@protonmail.com>
2025-01-15 14:52:35 +0100

    challenge nesting done

--- react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>1st button</Button>
+      <Button>2nd button</Button>
+      <Button>3rd button</Button>
+      <Button>4th button</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
