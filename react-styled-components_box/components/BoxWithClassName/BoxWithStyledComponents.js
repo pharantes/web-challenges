e73430aac96fae40b4d82bdce472fffa8529eb31
pharantes <p.arantes@protonmail.com>
2025-01-29 13:39:38 +0100

    styled components challenges initial commit

---
+++ react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
+  margin: 2rem;
+  &:hover {
+  background-color: red;
+  }
+`
