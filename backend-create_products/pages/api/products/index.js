62ad39d8ff5af8dc9f415562784575604b93f952
Pedro <p.arantes@protonmail.com>
2025-02-11 16:17:04 +0100

    challenge done

--- backend-create_products/pages/api/products/index.js
+++ backend-create_products/pages/api/products/index.js
@@ -10,6 +10,17 @@ export default async function handler(request, response) {
     response.status(200).json(products);
     return;
   }
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+
+      response.status(201).json({ status: "Product created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 
   response.status(405).json({ status: "Method not allowed." });
 }
